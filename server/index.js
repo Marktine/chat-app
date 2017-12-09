1a46ae59d8b646ca12bbf55fcf8c17a262d07821
cuongtm6 <cuongtm6@fsoft.com.vn>
2017-12-09 14:02:50 +0700

    Update

--- server/index.js
+++ server/index.js
@@ -9,12 +9,30 @@ let io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
+let connectedUsers = [];
+
 io.on('connection', (socket) => {
   console.log('user connected');
   socket.on('send-message', (message) => {
-    if(message !== '' && message !== null)
-    console.log(message);
+    if(message !== 'undefined' && message !== null)
+    {
+      console.log('User: ' + message.username + ' says: ' + message.message);
+      connectedUsers.push(Object.assign({
+        socketId: socket,
+        username: message.username,
+      }));
     io.emit('send-message', message);
+    }
+  })
+  socket.on('disconnect', () => {
+    let userIndex = connectedUsers.findIndex(user => user.socketId === socket)
+    let disconnectedUser = connectedUsers[userIndex];
+    if(disconnectedUser)
+    {
+      console.log(disconnectedUser.username + ' has disconnected.');
+      io.emit('user-disconnect',disconnectedUser.username + ' has disconnected.');
+      connectedUsers.splice(userIndex, 1);
+    }
   })
 });
 
